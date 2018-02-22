61ca8d41d909c9c8b283dc47b9a22a625a638a8e
Asma Laaribi <laaribi.asm@gmail.com>
2018-02-22 15:23:05 -0500

    completed the controllers actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+user["drinks"].each do |row|
+  name = row["strIngredient1"]
+  Ingredient.create(name: name)
+end
